fcf8f7f3b8bbb2d2c643595429a8835ad12b9f9f
vthakur0905 <91193422+vthakur0905@users.noreply.github.com>
2023-11-09 10:19:03 +0530

    Add files via upload

---
+++ B2_String_and_stringBuilder/A1_Strings_Basics.java
@@ -0,0 +1,19 @@
+package B2_String_And_StringBuilder;
+
+public class A1_Strings_Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name1 ="Vaibhav";
+		
+//		String name2 ="Vaibhav";
+		
+		System.out.println(name1.charAt(5));
+		
+		
+		
+		
+	}
+
+}
