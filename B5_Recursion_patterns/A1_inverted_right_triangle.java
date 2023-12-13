fe7ac6b09252003f773044cb14fcafa353cfe3b1
vthakur0905 <vthakur0905@gmail.com>
2023-12-13 21:23:27 +0530

    initial commit

---
+++ B5_Recursion_patterns/A1_inverted_right_triangle.java
@@ -0,0 +1,23 @@
+package B5_Recursion_patterns;
+
+public class A1_inverted_right_triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+	
+	static void triangle(int n , int i) {
+		
+		if (i == n) {
+			return ;
+		}
+		
+		
+		
+		
+	}
+
+}
